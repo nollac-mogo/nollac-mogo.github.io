e1a5d2da9fcb9241ce76e67b25955bc58659405e
nollac <alexandr7sheva@gmail.com>
2017-03-17 15:05:22 +0200

    upd

--- js/common.js
+++ js/common.js
@@ -3,11 +3,14 @@ $('.quote-slider, .work-slider').slick({
   respondTo: window
 });
 
-
 $('.navbar_link').on('click', function(){
     var target = $(this).attr('data-target'); //should be css selector (e.g. data-target=".next-block")
     $('html, body').animate({
         scrollTop: $(target).offset().top
     }, 1000);
     return false;
+});
+
+$('.mob_menu').on('click', function() {
+	$('.navbar').toggleClass('active');
 });
